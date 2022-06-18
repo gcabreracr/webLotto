ed56533a21880f250fe5b10e76c5730e9f4206ce
Gonzalo Cabrera <gcabreracr@gmail.com>
2022-06-18 11:01:44 -0600

    Commit PCRV 18jun 11am

---
+++ Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+const URL_BASE = 'http://localhost/lottoBanca';
+
+
+/***************************
+ * CONSTANTES DE BASE DATOS  
+ * ************************/
+
+ define('DB_NAME','');
+
+
+
+
+
