a7fcb5d05bffdcba574c3bbac4f20aed787949ca
Gonzalo Cabrera <gcabreracr@gmail.com>
2022-06-18 08:31:07 -0600

    Primer Commit

---
+++ Config/App/Views.php
@@ -0,0 +1,15 @@
+<?php
+class Views{
+
+    public function getView($controlador,$vista,$date=''){
+
+        $controlador = get_class($controlador);
+
+        if($controlador=='Home'){
+            $vista='Views/'.$vista.'php';            
+        }else{
+            $vista='Views/'.$controlador.'/'.$vista.'php';   
+        }
+        require_once $vista;
+    }
+}
